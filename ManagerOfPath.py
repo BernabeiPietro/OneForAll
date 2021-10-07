41ea9496bec54e09c2f7341a874e4e356754fd88
JackDettoLoco <unicoesolo94@gmail.com>
2021-10-07 23:26:44 +0200

    tolti i doppi // nei path e aggiustato 7z che non estraeva il file giusto nella cartella giusta

--- ManagerOfPath.py
+++ ManagerOfPath.py
@@ -10,14 +10,14 @@ class ManagerOfPath:
         self.path_figaro = path_base + "/FIGARO/"
         self.path_picrust = path_base + "/PICRUSt2/"
         self.path_fastqc = path_base + "/FASTQC/"
-        self.path_fqc_txt = self.path_fastqc+ "/report/"
+        self.path_fqc_txt = self.path_fastqc+ "report/"
         self.project_name = project_name
-        self.seq_qza = self.path_qiime + "/sample_seq-" + project_name + ".qza"
-        self.seq_qzv = self.path_qiime + "/sample_seq-" + project_name + ".qzv"
-        self.rep_qza = self.path_qiime + "/rep_seq-" + project_name + ".qza"
-        self.ASV_qza = self.path_qiime + "/ASV-" + project_name + ".qza"
-        self.DenStat_qza = self.path_qiime + "/denoising_stat-" + project_name + ".qza"
-        self.Tax = self.path_qiime + "/taxonomy-" + project_name + ".qza"
+        self.seq_qza = self.path_qiime + "sample_seq-" + project_name + ".qza"
+        self.seq_qzv = self.path_qiime + "sample_seq-" + project_name + ".qzv"
+        self.rep_qza = self.path_qiime + "rep_seq-" + project_name + ".qza"
+        self.ASV_qza = self.path_qiime + "ASV-" + project_name + ".qza"
+        self.DenStat_qza = self.path_qiime + "denoising_stat-" + project_name + ".qza"
+        self.Tax = self.path_qiime + "taxonomy-" + project_name + ".qza"
 
         os.mkdir(self.path_qiime)
         os.mkdir(self.path_cutadapt)
--- fastQC.py
+++ fastQC.py
@@ -45,10 +45,10 @@ def count_equals(median):
 
 #fastqc -o path -t tread inputpath --nogroup
 def run_fastqc(md,mp):
-    os.system("fastqc -o "+mp.path_fastqc+" -t "+md["tread"]+" "+mp.path_datainput+"/*.fastq --nogroup --noextract \
+    os.system("fastqc -o "+mp.path_fastqc+" -t "+md["tread"]+" "+mp.path_datainput+"/*/*.fastq --nogroup --noextract \
     && \
-    7z e "+mp.path_fastqc+"/*.zip -o"+mp.path_fqc_txt+" fastqc_data.txt -aou ")
-    dir_element = filter(lambda x: (".txt" in x), os.listdir(mp.path_fastqc))
+    7z e "+mp.path_fastqc+"*.zip -o"+mp.path_fqc_txt+" fastqc_data.txt -aou ")
+    dir_element = filter(lambda x: (".txt" in x), os.listdir(mp.path_fqc_txt))
     result = []
     for name_of_file in dir_element:
         result.append(parser_table(open(mp.path_fastqc + "\\" + name_of_file, "r")))

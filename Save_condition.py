fde176425dad4e16a0c25abe5f0d497f890de500
JackDettoLoco <unicoesolo94@gmail.com>
2021-10-09 23:31:26 +0200

    aggiunto intent per stampare il file meglio e forse aggiustato il path su dove stampare

--- Save_condition.py
+++ Save_condition.py
@@ -4,12 +4,15 @@
 #def save_cond_mp(mp):
  #   with open('path_set.txt', 'w') as data:
   #      data.write(str(mp))
-
+import os
 import json
 
-def save_cond_md(md):
-    with open("config_file.txt", 'w') as file:
-        file.write(json.dumps(md))
+def save_cond_md(md,mp):
+    p = mp.path_base
+    file_name = "config_file.txt"
+    cn = os.path.join(p,file_name)
+    with open(cn, 'w') as file:
+        file.write(json.dumps(md, indent=10))
 
 #da fare un file apposta per far leggere li impostazioni da file
 #def read_cond_md(md)# use `json.loads` to do the reverse
